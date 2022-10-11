e0deb38c8c05d28e24688455b458f2b7b7f68831
Jason D'Oyley <jdfdoyley@gmail.com>
2022-10-11 07:15:48 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿/*******************************************************************************
+ * Name: Jason D. F. D'Oyley
+ * Date: Oct. 11, 2022
+ * Assignment CIS214 Week 2 GP - Decisions & Logical Operators
+ *
+ * Main application (program) class.
+ * In this application we will demonstrate how logical operators function by
+ * printing "truth" tables to show the results of various logical and
+ * conditional operators. We will also explore decision making through
+ * the use of both an if-else statement and a switch statement.
+*/
---
+++ README.md
@@ -0,0 +1,7 @@
+# Decision & Logical Operators in C-Sharp
+
+Main application (program) class.
+In this application we will demonstrate how logical operators function by
+printing "truth" tables to show the results of various logical and
+conditional operators. We will also explore decision making through
+the use of both an if-else statement and a switch statement.
